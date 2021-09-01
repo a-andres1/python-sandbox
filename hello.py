bb103b0194a9845315b89c53ee0d274008020883
alyssa andres <alyssaandres1@gmail.com>
2021-08-31 23:14:24 -0400

    random number list & for loop, with comments

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
 import random
 n = random.random()
 print(n)
+
+#a for loop that repeats until it has 5 integers
+#created empty list
+randomlist = [ ]
+#for loop, declaring how many numbers you should go to
+for i in range(0,5):
+#pick a number between 1 and 40
+    x = random.randint(1,40)
+    #append the numbers to the list randomlist
+    randomlist.append(x)
+    #print the list
+    print(randomlist)
