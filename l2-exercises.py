848a3043916b19be2b22a5b9d8fac7c02b7e9d84
xena <74474948+a-andres1@users.noreply.github.com>
2022-07-15 11:32:02 -0400

    completed exercise 2, explanation in notes

--- l2-exercises.py
+++ l2-exercises.py
@@ -1,5 +1,18 @@
-# exercise 1 - uses input to promptt a user for their name and welcome user
+# exercise 1 - uses input to prompt a user for their name and welcome user
 
 print("What is your name?")
 inp = input()
 print("Whaddup, " + inp)
+
+# exercise 2 - small script that prompts user for rate & hours, then does the math for them. I think I will try the round function...
+print("What is your name?")
+name = input()
+print("How much do you make per hour?")
+rate = int(input())
+print("How many hours did you work?")
+hours = int(input())
+
+total_pay = round(hours * rate)
+
+print("Hey, " + name + "! You worked " + str(hours) +
+      " hours and made $" + str(total_pay) + ".")
