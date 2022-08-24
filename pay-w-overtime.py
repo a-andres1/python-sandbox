9638f5a062075102d796bb5a7708fd39de74156b
alyssa andres <alyssaandres1@gmail.com>
2022-08-24 10:49:56 -0500

    conditional statement works just fine, calculations are correct

--- pay-w-overtime.py
+++ pay-w-overtime.py
@@ -6,12 +6,14 @@ print("How much do you make per hour?")
 rate = float(input())
 print("How many hours did you work?")
 hours = float(input())
+
+# conditional - works just fine!
 if hours <= 40 :
     total_pay = round(hours * rate)
 else :
     if hours > 40 :
         overtime = hours - 40
-        base_pay = ( 40 * rate)
+        base_pay = (40 * rate)
         overtime_pay = (overtime * (rate * 1.5))
         total_pay = (overtime_pay + base_pay)
 
