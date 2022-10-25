e09ab590cd0313338c26abe470f13ca07048c5ea
alyssa andres <alyssaandres1@gmail.com>
2022-10-25 09:39:46 -0400

    added try/except

--- pay-w-overtime.py
+++ pay-w-overtime.py
@@ -19,3 +19,30 @@ else :
 
 print("Hey, " + name + "! You worked " + str(hours) +
       " hours and made $" + str(total_pay) + ".")
+
+
+# exercise 2 - write exercise 1 with try/except
+print("What is your name?")
+name = input()
+try:
+    print("How much do you make per hour?")
+    rate = float(input())
+    print("How many hours did you work?")
+    hours = float(input()) 
+    if hours <= 40 :
+        total_pay = round(hours * rate)
+    elif hours > 40 :
+            overtime = hours - 40
+            base_pay = (40 * rate)
+            overtime_pay = (overtime * (rate * 1.5))
+            total_pay = (overtime_pay + base_pay)
+            
+except ValueError:
+    print("please enter a number")
+
+print("Hey, " + name + "! You worked " + str(hours) +
+      " hours and made $" + str(total_pay) + ".")
+
+
+
+
